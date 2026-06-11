cf35f58bfbbf8e5fc65a0cbcaa078fa152dc5809
Leonardo de Oliveira Moreira <77798324+LeoOliveiraLS@users.noreply.github.com>
2026-06-11 17:48:53 -0300

    Update cetus-monitor_tg2.js

--- cetus-monitor_tg2.js
+++ cetus-monitor_tg2.js
@@ -5,8 +5,8 @@ const RPC_URL      = "https://fullnode.mainnet.sui.io";
 const CETUS_API    = "https://api-sui.cetus.zone";
 
 // ─── CONFIGURE AQUI ──────────────────────────────────────────────────────────
-const POSITION_ID      = "0x863b5ba15f28cd2d646eab0be9bcb9e765885ebc31e788c1ceb0db5bced766a4";
-const POSITION_CREATED = 1781207983725; // timestamp de criação em ms
+const POSITION_ID      = "0xc3c526fe485f2900b72b06ea3e7f20a7905d28e1c98fb67f3e350b08c5c743e4";
+const POSITION_CREATED = 1781210903597; // timestamp de criação em ms
 const INTERVAL_MS      = 30_000;
 const TG_TOKEN         = "8768478427:AAEyBm8woaPapJOvRvRmdSroK0i-GGAUClo";
 const TG_CHAT_ID       = "776460062";
